9c568f71669c69f8d9be0e56b2befb54822f39a0
Anthony Weathersby <anthonycweathersby@gmail.com>
2018-11-24 21:08:53 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CandleFW URL
---
+++ url.mjs
@@ -0,0 +1,586 @@
+import whind from "@candlefw/whind";
+
+const uri_reg_ex = /(?:([^\:\?\[\]\@\/\#\b\s][^\:\?\[\]\@\/\#\b\s]*)(?:\:\/\/))?(?:([^\:\?\[\]\@\/\#\b\s][^\:\?\[\]\@\/\#\b\s]*)(?:\:([^\:\?\[\]\@\/\#\b\s]*)?)?\@)?(?:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|((?:\[[0-9a-f]{1,4})+(?:\:[0-9a-f]{0,4}){2,7}\])|([^\:\?\[\]\@\/\#\b\s\.]{2,}(?:\.[^\:\?\[\]\@\/\#\b\s]*)*))?(?:\:(\d+))?((?:[^\?\[\]\#\s\b]*)+)?(?:\?([^\[\]\#\s\b]*))?(?:\#([^\#\s\b]*))?/i;
+
+const STOCK_LOCATION = {
+    protocol :"",
+    host :"",
+    port :"",
+    path :"",
+    hash :"",
+    query :"",
+    search:""
+}
+
+function fetchLocalText(URL, m = "same-origin") {
+    return new Promise((res, rej) => {
+        fetch(URL, {
+            mode: m, // CORs not allowed
+            credentials: m,
+            method: "Get"
+        }).then(r => {
+            if (r.status < 200 || r.status > 299)
+                r.text().then(rej);
+            else
+                r.text().then(res);
+        }).catch(e => rej(e));
+    });
+}
+
+function fetchLocalJSON(URL, m = "same-origin") {
+    return new Promise((res, rej) => {
+        fetch(URL, {
+            mode: m, // CORs not allowed
+            credentials: m,
+            method: "Get"
+        }).then(r => {
+            if (r.status < 200 || r.status > 299)
+                r.json().then(rej);
+            else
+                r.json().then(res).catch(rej);
+        }).catch(e => rej(e));
+    });
+}
+
+function submitForm(URL, form_data, m = "same-origin") {
+    return new Promise((res, rej) => {
+        var form;
+
+        if (form_data instanceof FormData)
+            form = form_data;
+        else {
+            form = new FormData();
+            for (let name in form_data)
+                form.append(name, form_data[name] + "");
+        }
+
+        fetch(URL, {
+            mode: m, // CORs not allowed
+            credentials: m,
+            method: "POST",
+            body: form,
+        }).then(r => {
+            if (r.status < 200 || r.status > 299)
+                r.text().then(rej);
+            else
+                r.json().then(res);
+        }).catch(e => e.text().then(rej));
+    });
+}
+
+function submitJSON(URL, json_data, m = "same-origin") {
+    return new Promise((res, rej) => {
+        fetch(URL, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            mode: m, // CORs not allowed
+            credentials: m,
+            method: "POST",
+            body: JSON.stringify(json_data),
+        }).then(r => {
+            if (r.status < 200 || r.status > 299)
+                r.json().then(rej);
+            else
+                r.json().then(res);
+        }).catch(e => e.text().then(rej));
+    });
+}
+
+
+
+/**
+ * Used for processing URLs, handling `document.location`, and fetching data.
+ * @param      {string}   url           The URL string to wrap.
+ * @param      {boolean}  USE_LOCATION  If `true` missing URL parts are filled in with data from `document.location`. 
+ * @return     {URL}   If a falsy value is passed to `url`, and `USE_LOCATION` is `true` a Global URL is returned. This is directly linked to the page and will _update_ the actual page URL when its values are change. Use with caution. 
+ * @alias URL
+ * @memberof module:wick.core.network
+ */
+export class URL {
+
+    static resolveRelative(URL_or_url_original, URL_or_url_new) {
+        
+        let URL_old = (URL_or_url_original instanceof URL) ? URL_or_url_original : new URL(URL_or_url_original);
+        let URL_new = (URL_or_url_new instanceof URL) ? URL_or_url_new : new URL(URL_or_url_new);
+
+        let new_path = "";
+        if (URL_new.path[0] != "/") {
+
+            let a = URL_old.path.split("/");
+            let b = URL_new.path.split("/");
+
+
+            if (b[0] == "..") a.splice(a.length - 1, 1);
+            for (let i = 0; i < b.length; i++) {
+                switch (b[i]) {
+                    case "..":
+                    case ".":
+                        a.splice(a.length - 1, 1);
+                        break;
+                    default:
+                        a.push(b[i]);
+                }
+            }
+            URL_new.path = a.join("/");
+        }
+
+
+        return URL_new;
+    }
+
+    constructor(url = "", USE_LOCATION = false) {
+
+        let IS_STRING = true;
+        
+
+        const location = (typeof(document) !== "undefined") ? document.location : STOCK_LOCATION;
+
+        if (!url || typeof(url) != "string") {
+            IS_STRING = false;
+            if (URL.GLOBAL && USE_LOCATION)
+                return URL.GLOBAL;
+        }
+
+        /**
+         * URL protocol
+         */
+        this.protocol = "";
+
+        /**
+         * Username string
+         */
+        this.user = "";
+
+        /**
+         * Password string
+         */
+        this.pwd = "";
+
+        /**
+         * URL hostname
+         */
+        this.host = "";
+
+        /**
+         * URL network port number.
+         */
+        this.port = 0;
+
+        /**
+         * URL resource path
+         */
+        this.path = "";
+
+        /**
+         * URL query string.
+         */
+        this.query = "";
+
+        /**
+         * Hashtag string
+         */
+        this.hash = "";
+
+        /**
+         * Map of the query data
+         */
+        this.map = null;
+
+        if (IS_STRING) {
+            if (url instanceof URL) {
+                this.protocol = url.protocol;
+                this.user = url.user;
+                this.pwd = url.pwd;
+                this.host = url.host;
+                this.port = url.port;
+                this.path = url.path;
+                this.query = url.query;
+                this.hash = url.hash;
+            } else {
+                let part = url.match(uri_reg_ex);
+                this.protocol = part[1] || ((USE_LOCATION) ? location.protocol : "");
+                this.user = part[2] || "";
+                this.pwd = part[3] || "";
+                this.host = part[4] || part[5] || part[6] || ((USE_LOCATION) ? location.hostname : "");
+                this.port = parseInt(part[7] || ((USE_LOCATION) ? location.port : 0));
+                this.path = part[8] || ((USE_LOCATION) ? location.pathname : "");
+                this.query = part[9] || ((USE_LOCATION) ? location.search.slice(1) : "");
+                this.hash = part[10] || ((USE_LOCATION) ? location.hash.slice(1) : "");
+            }
+        } else if (USE_LOCATION) {
+
+            URL.G = this;
+            this.protocol = location.protocol;
+            this.host = location.hostname;
+            this.port = location.port;
+            this.path = location.pathname;
+            this.hash = location.hash.slice(1);
+            this.query = location.search.slice(1);
+            this._getQuery_(this.query);
+
+            return URL.R;
+        }
+        this._getQuery_(this.query);
+    }
+
+
+    /**
+    URL Query Syntax
+
+    root => [root_class] [& [class_list]]
+         => [class_list]
+
+    root_class = key_list
+
+    class_list [class [& key_list] [& class_list]]
+
+    class => name & key_list
+
+    key_list => [key_val [& key_list]]
+
+    key_val => name = val
+
+    name => ALPHANUMERIC_ID
+
+    val => NUMBER
+        => ALPHANUMERIC_ID
+    */
+
+    /**
+     * Pulls query string info into this.map
+     * @private
+     */
+    _getQuery_() {
+        let map = (this.map) ? this.map : (this.map = new Map());
+
+        let lex = whind(this.query);
+
+        const get_map = (k, m) => (m.has(k)) ? m.get(k) : m.set(k, new Map).get(k);
+
+        let key = 0,
+            key_val = "",
+            class_map = get_map(key_val, map),
+            lfv = 0;
+
+        while (!lex.END) {
+            switch (lex.tx) {
+                case "&": //At new class or value
+                    if (lfv > 0)
+                        key = (class_map.set(key_val, lex.s(lfv)), lfv = 0, lex.n().pos);
+                    else {
+                        key_val = lex.s(key);
+                        key = (class_map = get_map(key_val, map), lex.n().pos);
+                    }
+                    continue;
+                case "=":
+                    //looking for a value now
+                    key_val = lex.s(key);
+                    lfv = lex.n().pos;
+                    continue;
+            }
+            lex.n();
+        }
+
+        if (lfv > 0) class_map.set(key_val, lex.s(lfv));
+    }
+
+    setPath(path) {
+
+        this.path = path;
+
+        return new URL(this);
+    }
+
+    setLocation() {
+        history.replaceState({}, "replaced state", `${this}`);
+        window.onpopstate();
+    }
+
+    toString() {
+        let str = [];
+
+        if (this.protocol && this.host)
+            str.push(`${this.protocol}://`);
+
+        if (this.host)
+            str.push(`${this.host}`);
+
+        if (this.port)
+            str.push(`:${this.port}`);
+
+        if (this.path)
+            str.push(`${this.path[0] == "/" ? "" : "/"}${this.path}`);
+
+        if (this.query)
+            str.push(this.query);
+
+        return str.join("");
+    }
+
+    /**
+     * Pulls data stored in query string into an object an returns that.
+     * @param      {string}  class_name  The class name
+     * @return     {object}  The data.
+     */
+    getData(class_name = "") {
+        if (this.map) {
+            let out = {};
+            let _c = this.map.get(class_name);
+            if (_c) {
+                for (let [key, val] of _c.entries())
+                    out[key] = val;
+                return out;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Sets the data in the query string. Wick data is added after a second `?` character in the query field, and appended to the end of any existing data.
+     * @param      {string}  class_name  Class name to use in query string. Defaults to root, no class 
+     * @param      {object | Model | AnyModel}  data        The data
+     */
+    setData(data = null, class_name = "") {
+
+        if (data) {
+
+            let map = this.map = new Map();
+
+            let store = (map.has(class_name)) ? map.get(class_name) : (map.set(class_name, new Map()).get(class_name));
+
+            //If the data is a falsy value, delete the association.
+
+            for (let n in data) {
+                if (data[n] !== undefined && typeof data[n] !== "object")
+                    store.set(n, data[n]);
+                else
+                    store.delete(n);
+            }
+
+            //set query
+            let class_, null_class, str = "";
+
+            if ((null_class = map.get(""))) {
+                if (null_class.size > 0) {
+                    for (let [key, val] of null_class.entries())
+                        str += `&${key}=${val}`;
+
+                }
+            }
+
+            for (let [key, class_] of map.entries()) {
+                if (key === "")
+                    continue;
+                if (class_.size > 0) {
+                    str += `&${key}`
+                    for (let [key, val] of class_.entries())
+                        str += `&${key}=${val}`;
+                }
+            }
+
+            str = str.slice(1);
+
+            this.query = this.query.split("?")[0] + "?" + str;
+
+            if (URL.G == this)
+                this.goto();
+        } else {
+            this.query = "";
+        }
+
+        return this;
+
+    }
+
+    /**
+     * Fetch a string value of the remote resource. 
+     * Just uses path component of URL. Must be from the same origin.
+     * @param      {boolean}  [ALLOW_CACHE=true]  If `true`, the return string will be cached. If it is already cached, that will be returned instead. If `false`, a network fetch will always occur , and the result will not be cached.
+     * @return     {Promise}  A promise object that resolves to a string of the fetched value.
+     */
+    fetchText(ALLOW_CACHE = true) {
+
+        if (ALLOW_CACHE) {
+
+            let resource = URL.RC.get(this.path);
+
+            if (resource)
+                return new Promise((res) => {
+                    res(resource);
+                });
+        }
+
+        return fetchLocalText(this.path).then(res => (URL.RC.set(this.path, res), res));
+    }
+
+    /**
+     * Fetch a JSON value of the remote resource. 
+     * Just uses path component of URL. Must be from the same origin.
+     * @param      {boolean}  [ALLOW_CACHE=true]  If `true`, the return string will be cached. If it is already cached, that will be returned instead. If `false`, a network fetch will always occur , and the result will not be cached.
+     * @return     {Promise}  A promise object that resolves to a string of the fetched value.
+     */
+    fetchJSON(ALLOW_CACHE = true) {
+
+        let string_url = this.toString();
+
+        if (ALLOW_CACHE) {
+
+            let resource = URL.RC.get(string_url);
+
+            if (resource)
+                return new Promise((res) => {
+                    res(resource);
+                });
+        }
+
+        return fetchLocalJSON(string_url).then(res => (URL.RC.set(this.path, res), res));
+    }
+
+    /**
+     * Cache a local resource at the value 
+     * @param    {object}  resource  The resource to store at this URL path value.
+     * @returns {boolean} `true` if a resource was already cached for this URL, false otherwise.
+     */
+    cacheResource(resource) {
+
+        let occupied = URL.RC.has(this.path);
+
+        URL.RC.set(this.path, resource);
+
+        return occupied;
+    }
+
+    submitForm(form_data) {
+        return submitForm(this.toString(), form_data);
+    }
+
+    submitJSON(json_data) {
+            return submitJSON(this.toString(), json_data);
+        }
+        /**
+         * Goes to the current URL.
+         */
+    goto() {
+        return;
+        let url = this.toString();
+        history.pushState({}, "ignored title", url);
+        window.onpopstate();
+        URL.G = this;
+    }
+
+    get pathname() {
+        return this.path;
+    }
+
+    get href() {
+        return this.toString();
+    }
+}
+
+/**
+ * The fetched resource cache.
+ */
+URL.RC = new Map();
+
+/**
+ * The Default Global URL object. 
+ */
+URL.G = null;
+
+/**
+ * The Global object Proxy.
+ */
+URL.R = {
+    get protocol() {
+        return URL.G.protocol;
+    },
+    set protocol(v) {
+        return;
+        URL.G.protocol = v;
+    },
+    get user() {
+        return URL.G.user;
+    },
+    set user(v) {
+        return;
+        URL.G.user = v;
+    },
+    get pwd() {
+        return URL.G.pwd;
+    },
+    set pwd(v) {
+        return;
+        URL.G.pwd = v;
+    },
+    get host() {
+        return URL.G.host;
+    },
+    set host(v) {
+        return;
+        URL.G.host = v;
+    },
+    get port() {
+        return URL.G.port;
+    },
+    set port(v) {
+        return;
+        URL.G.port = v;
+    },
+    get path() {
+        return URL.G.path;
+    },
+    set path(v) {
+        return;
+        URL.G.path = v;
+    },
+    get query() {
+        return URL.G.query;
+    },
+    set query(v) {
+        return;
+        URL.G.query = v;
+    },
+    get hash() {
+        return URL.G.hash;
+    },
+    set hash(v) {
+        return;
+        URL.G.hash = v;
+    },
+    get map() {
+        return URL.G.map;
+    },
+    set map(v) {
+        return;
+        URL.G.map = v;
+    },
+    setPath(path) {
+        return URL.G.setPath(path);
+    },
+    setLocation() {
+        return URL.G.setLocation();
+    },
+    toString() {
+        return URL.G.toString();
+    },
+    getData(class_name = "") {
+        return URL.G.getData(class_name = "");
+    },
+    setData(class_name = "", data = null) {
+        return URL.G.setData(class_name, data);
+    },
+    fetchText(ALLOW_CACHE = true) {
+        return URL.G.fetchText(ALLOW_CACHE);
+    },
+    cacheResource(resource) {
+        return URL.G.cacheResource(resource);
+    }
+};
+Object.freeze(URL.R);
+Object.freeze(URL.RC);
+Object.seal(URL);
+
+
+export default URL;
